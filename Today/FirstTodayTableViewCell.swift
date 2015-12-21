249df195a84bf25260431acc9c7c890376314664
Masaichi Ueta <masamichi1023@gmail.com>
2015-12-21 22:09:48 +0900

    #5 Add tableviewcell class

---
+++ Today/FirstTodayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FirstTodayTableViewCell.swift
+//  Today
+//
+//  Created by UetaMasamichi on 2015/12/21.
+//  Copyright © 2015年 Masamichi Ueta. All rights reserved.
+//
+
+import UIKit
+
+class FirstTodayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Today/TodayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodayTableViewCell.swift
+//  Today
+//
+//  Created by UetaMasamichi on 2015/12/21.
+//  Copyright © 2015年 Masamichi Ueta. All rights reserved.
+//
+
+import UIKit
+
+class TodayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

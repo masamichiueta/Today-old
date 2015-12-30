8a3bf8760700ce4f55b523e9f01eb2d220b08384
Masaichi Ueta <masamichi1023@gmail.com>
2015-12-30 17:54:54 +0900

    Add progress circle drawing function

--- Today/View/CircleView.swift
+++ Today/View/CircleView.swift
@@ -11,26 +11,44 @@ import UIKit
 
 @IBDesignable class CircleView: UIView {
 
-    @IBInspectable var borderColor: UIColor = UIColor.clearColor()
+    @IBInspectable var progressCircleColor: UIColor = UIColor.clearColor()
+    @IBInspectable var backCircleColor: UIColor = UIColor.blackColor()
+    @IBInspectable var layerOpacity: Double = 0.1
+    @IBInspectable var borderWidth: Double = 20.0
     
+    private let progressCircleLayer: CAShapeLayer = CAShapeLayer()
+    private let backCircleLayer: CAShapeLayer = CAShapeLayer()
+    private var circleCenter: CGPoint!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        circleCenter = CGPoint(
+            x: (self.frame.origin.x + self.frame.size.width/2.0),
+            y: (self.frame.origin.y + self.frame.size.height/2.0))
     }
     
     override func prepareForInterfaceBuilder() {
-        self.layer.borderWidth = 2.0
-        self.layer.cornerRadius = self.frame.width / 2
-        self.clipsToBounds = true
-        self.layer.borderColor = borderColor.CGColor
+        drawBackCircle()
     }
     
 
     override func drawRect(rect: CGRect) {
-        self.layer.cornerRadius = self.frame.width / 2
-        self.clipsToBounds = true
-        self.layer.borderColor = borderColor.CGColor
+        drawBackCircle()
     }
-
+    
+    
+    func drawBackCircle() {
+        let path = UIBezierPath(
+            arcCenter: circleCenter,
+            radius: CGFloat(self.frame.size.width/2.0),
+            startAngle: CGFloat(0),
+            endAngle: CGFloat(M_PI * 2),
+            clockwise: true)
+        backCircleLayer.path = path.CGPath
+        backCircleLayer.strokeColor = backCircleColor.CGColor
+        backCircleLayer.opacity = Float(layerOpacity)
+        backCircleLayer.fillColor = nil
+        backCircleLayer.lineWidth = CGFloat(borderWidth)
+        self.layer.addSublayer(backCircleLayer)
+    }
 }

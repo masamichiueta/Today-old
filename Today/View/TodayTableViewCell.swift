32e7d6d48e15932ae75697845f9e576a6278d5ac
Masaichi Ueta <masamichi1023@gmail.com>
2016-01-02 16:28:21 +0900

    Add setting icon

--- Today/View/TodayTableViewCell.swift
+++ Today/View/TodayTableViewCell.swift
@@ -10,22 +10,22 @@ import UIKit
 import TodayModel
 
 class TodayTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var colorView: UIView!
     @IBOutlet weak var scoreLabel: UILabel!
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
 
 private let dateFormatter: NSDateFormatter = {

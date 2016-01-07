ac1becf640721aa7105f2ed20c909fae65491243
Masaichi Ueta <masamichi1023@gmail.com>
2016-01-07 23:38:30 +0900

    Modify tableview cell design

--- Today/Controller/AddTodayViewController.swift
+++ Today/Controller/AddTodayViewController.swift
@@ -13,30 +13,22 @@ import LTMorphingLabel
 class AddTodayViewController: UIViewController {
     
     @IBOutlet weak var pickerView: UIPickerView!
-    
     @IBOutlet weak var scoreCircleView: TodayScoreCircleView!
-    
     @IBOutlet weak var scoreLabel: LTMorphingLabel!
-    
-    @IBOutlet weak var wordLabel: LTMorphingLabel!
-    
     @IBOutlet weak var iconImageView: UIImageView!
     
     var score: Int =  Today.maxScore {
         didSet {
             scoreCircleView.score = score
             scoreLabel.morphingDuration = Float(scoreCircleView.animationDuration + 0.15)
-            wordLabel.morphingDuration = Float(scoreCircleView.animationDuration + 0.15)
             scoreLabel.text = "\(score)"
-            wordLabel.text = Today.type(score).rawValue
             let toStrokeColor = Today.type(score).color()
-            self.scoreLabel.textColor = toStrokeColor
-            self.wordLabel.textColor = toStrokeColor
+            scoreLabel.textColor = toStrokeColor
+            iconImageView.tintColor = toStrokeColor
             UIView.transitionWithView(iconImageView,
                 duration: scoreCircleView.animationDuration,
                 options: UIViewAnimationOptions.TransitionCrossDissolve,
                 animations: { [unowned self] in
-                    self.iconImageView.tintColor = toStrokeColor
                     self.iconImageView.image = Today.type(self.score).icon("40")
                     self.view.layoutIfNeeded()
                 },
@@ -48,32 +40,15 @@ class AddTodayViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         scoreLabel.morphingEffect = .Evaporate
         scoreLabel.textColor = scoreCircleView.progressCircleColor
-        wordLabel.morphingEffect = .Evaporate
-        wordLabel.textColor = scoreCircleView.progressCircleColor
-        
         iconImageView.image = Today.type(score).icon("40")
         iconImageView.tintColor = scoreCircleView.progressCircleColor
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
-    
 }
 
 extension AddTodayViewController: UIPickerViewDataSource, UIPickerViewDelegate {

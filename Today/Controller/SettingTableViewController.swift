53e862796df71a7c83fda2bdf279767d00f54d21
Masaichi Ueta <masamichi1023@gmail.com>
2016-01-09 02:15:50 +0900

    #13 Modify tableview cell design

--- Today/Controller/SettingTableViewController.swift
+++ Today/Controller/SettingTableViewController.swift
@@ -9,6 +9,30 @@
 import UIKit
 
 class SettingTableViewController: UITableViewController {
+    
+    private let dateFormatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .NoStyle
+        formatter.timeStyle = .ShortStyle
+        return formatter
+    }()
+    
+    private let pickerIndexPath = NSIndexPath(forRow: 2, inSection: 0)
+    
+    private var pickerHidden: Bool = true {
+        didSet {
+            if pickerHidden {
+                tableView.beginUpdates()
+                tableView.deleteRowsAtIndexPaths([pickerIndexPath], withRowAnimation: .Fade)
+                tableView.endUpdates()
+            } else {
+                tableView.beginUpdates()
+                tableView.insertRowsAtIndexPaths([pickerIndexPath], withRowAnimation: .Fade)
+                tableView.endUpdates()
+            }
+            
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +57,10 @@ class SettingTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0:
-            return 2
+            if pickerHidden {
+                return 2
+            }
+            return 3
         case 1:
             return 2
         default:
@@ -47,17 +74,54 @@ class SettingTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         switch (indexPath.section, indexPath.row) {
-        case (0,0):
-            guard let cell = tableView.dequeueReusableCellWithIdentifier("PickerCell", forIndexPath: indexPath) as? TodayPickerTableViewCell else {
+        case (0, 0):
+            let cell = tableView.dequeueReusableCellWithIdentifier("SwitchCell", forIndexPath: indexPath)
+            cell.textLabel?.text = "Notification Setting"
+            let sw = UISwitch()
+            cell.accessoryView = sw
+            return cell
+        case (0, 1):
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+            cell.textLabel?.text = "Notification Time"
+            cell.detailTextLabel?.text = dateFormatter.stringFromDate(NSDate())
+            return cell
+        case (pickerIndexPath.section, pickerIndexPath.row):
+            guard let cell = tableView.dequeueReusableCellWithIdentifier("PickerCell") as? TodayPickerTableViewCell else {
                 fatalError("Wrong cell type")
             }
+            cell.datePicker.date = NSDate()
+            cell.delegate = self
+            return cell
+        case (1, 0):
+            let cell = UITableViewCell(style: .Value1, reuseIdentifier: "cell")
+            cell.textLabel?.text = "Rate today app"
+            return cell
+        case (1, 1):
+            let cell = UITableViewCell(style: .Value1, reuseIdentifier: "cell")
+            cell.textLabel?.text = "Version"
+            cell.detailTextLabel?.text = "1.0"
             return cell
         default:
             break
         }
         
-        //let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
         let cell = UITableViewCell(style: .Value1, reuseIdentifier: "cell")
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        switch (indexPath.section, indexPath.row) {
+        case (pickerIndexPath.section, pickerIndexPath.row - 1):
+            pickerHidden = !pickerHidden
+        default:
+            break
+        }
+    }
+}
+
+extension SettingTableViewController: TodayPickerTableViewCellDelegate {
+    func dateDidChange(date: NSDate) {
+        
     }
 }

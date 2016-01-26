fc2a556780a723e5e95cab4ef04553ce6fb8df03
Masaichi Ueta <masamichi1023@gmail.com>
2016-01-27 00:08:03 +0900

    #26 Modify Today View

--- Today Watch App/Today Watch Extension/InterfaceController.swift
+++ Today Watch App/Today Watch Extension/InterfaceController.swift
@@ -13,21 +13,35 @@ import WatchConnectivity
 
 class InterfaceController: WKInterfaceController, WCSessionDelegate {
     
+    
     var session: WCSession!
+    
+    @IBOutlet var addGroup: WKInterfaceGroup!
+    
+    @IBOutlet var scoreGroup: WKInterfaceGroup!
+    
+    @IBOutlet var unreachableGroup: WKInterfaceGroup!
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
+        if WCSession.isSupported() {
+            session = WCSession.defaultSession()
+            session.delegate = self
+            session.activateSession()
+        }
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-        if WCSession.isSupported() {
-            session = WCSession.defaultSession()
-            session.delegate = self
-            session.activateSession()
+        
+        if session.reachable {
+            unreachableGroup.setHidden(true)
+        } else {
+            addGroup.setHidden(true)
+            scoreGroup.setHidden(true)
+
         }
     }
     
@@ -36,9 +50,9 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
         super.didDeactivate()
     }
     
-//    @IBAction func addToday() {
-//        
-//        if session.reachable {
+    @IBAction func addToday() {
+        
+        if session.reachable {
 //            session.sendMessage(["score": 10],
 //                replyHandler: {
 //                    (content: [String: AnyObject]) -> Void in
@@ -48,7 +62,8 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
 //                    (error) -> Void in
 //                    print("We got an error from our paired device : " + error.domain)
 //            })
-//        }
-//        
-//    }
+            presentController([(name: "test", context: self)])
+        }
+        
+    }
 }

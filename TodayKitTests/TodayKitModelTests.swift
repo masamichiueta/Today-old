c894f55129638b15bd3618eaf3f918020f6c13fd
Masamichi Ueta <masamichi1023@gmail.com>
2016-04-08 23:53:09 +0900

    #20 Add test case

--- TodayKitTests/TodayKitModelTests.swift
+++ TodayKitTests/TodayKitModelTests.swift
@@ -45,16 +45,12 @@ class TodayKitModelTests: XCTestCase {
             fatalError("ManagedObjectContext not found")
         }
         
-        guard let todayEntity = NSEntityDescription.entityForName(Today.entityName, inManagedObjectContext: managedObjectContext),
-        let streakEntity = NSEntityDescription.entityForName(Streak.entityName, inManagedObjectContext: managedObjectContext) else {
-            fatalError("Entity not found")
-        }
+        let todayEntity = NSEntityDescription.entityForName(Today.entityName, inManagedObjectContext: managedObjectContext)
+        XCTAssertNotNil(todayEntity)
         
-        let today = Today(entity: todayEntity, insertIntoManagedObjectContext: managedObjectContext)
-        XCTAssertNotNil(today)
+        let streakEntity = NSEntityDescription.entityForName(Streak.entityName, inManagedObjectContext: managedObjectContext)
+        XCTAssertNotNil(streakEntity)
         
-        let streak = Streak(entity: streakEntity, insertIntoManagedObjectContext: managedObjectContext)
-        XCTAssertNotNil(streak)
     }
     
     //MARK: - Today
@@ -76,14 +72,47 @@ class TodayKitModelTests: XCTestCase {
     }
     
     func testMasterScore() {
-        guard let max = Today.masterScores.maxElement(), min = Today.masterScores.minElement() else {
-            fatalError()
-        }
-        
-        XCTAssertEqual(max, 10)
-        XCTAssertEqual(min, 0)
+        XCTAssertEqual(Today.masterScores.count, 11)
+        XCTAssertEqual(Today.maxMasterScore, 10)
+        XCTAssertEqual(Today.minMasterScore, 0)
     }
     
+    
+    
+    func testTodayType() {
+        let poorType0 = Today.type(0)
+        XCTAssertEqual(poorType0, TodayType.Poor)
+        
+        let poorType1 = Today.type(1)
+        XCTAssertEqual(poorType1, TodayType.Poor)
+        
+        let poorType2 = Today.type(2)
+        XCTAssertEqual(poorType2, TodayType.Poor)
+        
+        let fairType3 = Today.type(3)
+        XCTAssertEqual(fairType3, TodayType.Fair)
+        
+        let fairType4 = Today.type(4)
+        XCTAssertEqual(fairType4, TodayType.Fair)
+        
+        let averageType5 = Today.type(5)
+        XCTAssertEqual(averageType5, TodayType.Average)
+        
+        let averageType6 = Today.type(6)
+        XCTAssertEqual(averageType6, TodayType.Average)
+        
+        let goodType7 = Today.type(7)
+        XCTAssertEqual(goodType7, TodayType.Good)
+        
+        let goodType8 = Today.type(8)
+        XCTAssertEqual(goodType8, TodayType.Good)
+        
+        let excellentType9 = Today.type(9)
+        XCTAssertEqual(excellentType9, TodayType.Excellent)
+        
+        let excellentType10 = Today.type(10)
+        XCTAssertEqual(excellentType10, TodayType.Excellent)
+    }
     
     
     //MARK: - Streak

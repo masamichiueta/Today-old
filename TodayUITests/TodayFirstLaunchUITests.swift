64e42e82257f637e6221b943aa588dac59ee6d4d
Masamichi Ueta <masamichi1023@gmail.com>
2016-04-25 00:25:45 +0900

    #21 Modify launch argument

--- TodayUITests/TodayFirstLaunchUITests.swift
+++ TodayUITests/TodayFirstLaunchUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Today
 @testable import TodayKit
 
 class TodayFirstLaunchUITests: XCTestCase {
@@ -20,7 +21,8 @@ class TodayFirstLaunchUITests: XCTestCase {
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         let app = XCUIApplication()
-        app.launchArguments = ["firstLaunchUITest"]
+        app.launchArguments = ["FirstLaunchUITest"]
+
         app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
